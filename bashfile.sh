64d7749e95e7b5a3b8935925037f9f9c968eb4fc
Ubuntu <ubuntu@ip-172-31-43-229.eu-north-1.compute.internal>
2023-11-25 10:07:14 +0000

    sec commit

---
+++ bashfile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir backend2
+touch deploy3.sh
+vi deploy3.sh
+
+
+git add .
+git commit -m "second commit"
+git push origin main
+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
+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
